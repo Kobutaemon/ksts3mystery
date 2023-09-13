58beb6f1b2a67f9d93f785e460c463169b18f50a
Kobutaemon <77833661+Kobutaemon@users.noreply.github.com>
2023-09-13 13:58:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-window.document.onkeydown = (e) => {
-  	if (e.keyCode === 13) {
-        let element = document.getElementById("answer");
-        if (element.value.toLowerCase() === "kst"){ //見なかったことにして謎を解いてね
-            alert("success");
-        }
-	}  
+let element = document.getElementById("answer");
+let message = document.getElementById("message");
+
+function messageError() {
+  message.classList.remove("d-none");
+  message.innerText = "認証失敗";
+  setTimeout(function() {
+    message.classList.add("d-none");
+  }, 4000);
 }
 
-const check_word = () => {
-    let element = document.getElementById("answer");
-    if (element.value.toLowerCase() === "kst"){ //見なかったことにして謎を解いてね
+window.document.onkeydown = (e) => {
+  if (e.keyCode === 13) {
+    switch (element.value.toLowerCase()) {
+      case "kst":
         alert("success");
+        break;
+
+      case "drp":
+        alert("success");
+        break;
+
+      default:
+        messageError();
     }
+  }  
 }
+
+function check_word() {
+  switch (element.value.toLowerCase()) {
+    case "kst":
+      alert("success");
+      break;
+
+    case "drp":
+      alert("success");
+      break;
+
+    default:
+      messageError();
+  }
+}  
